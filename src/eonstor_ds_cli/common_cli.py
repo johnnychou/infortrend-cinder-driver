6e81685ba2bef32c4de15c3ac34b54e9834eea1f
James.Tsai <jameslovee10@gmail.com>
2015-07-08 14:53:33 +0800

    fix _select_most_free_capacity_pool_id bug
    Signed-off-by: James.Tsai <jameslovee10@gmail.com>

--- src/eonstor_ds_cli/common_cli.py
+++ src/eonstor_ds_cli/common_cli.py
@@ -683,7 +683,7 @@ class InfortrendCommon(object):
             for lv in lv_info:
                 # LOG.info(_LI('Loop Tier %(tier)s'), {'tier': lv['Name']})
                 if (lv['Name'] in self.tier_pools_dict.keys() and
-                        tier_level >= (max(
+                        tier_level <= (max(
                             self.tier_pools_dict[lv['Name']]) + 1)):
                     available_space = float(lv['Available'].split(' ', 1)[0])
                     free_capacity_gb = round(mi_to_gi(available_space))
